9dff55e255dfb3841d511058b541946b78c6d24b
nieminendesign <info@nieminen.design>
2025-07-06 02:13:31 +0300

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,1 +1,9 @@
 // Firebase config, täytä .env arvoilla
+export const firebaseConfig = {
+  apiKey: "AIzaSyDIL8FXArmC8Sfuc50-wds_jJPRvyb9Hro",
+  authDomain: "tuloscortti.firebaseapp.com",
+  projectId: "tuloscortti",
+  storageBucket: "tuloscortti.firebasestorage.app",
+  messagingSenderId: "802429838755",
+  appId: "1:802429838755:web:344188713798c01d7f2616"
+};
