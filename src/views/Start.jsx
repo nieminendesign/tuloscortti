b77d740a595ca7c13cfaca4d33e5bbea572b9e6c
nieminendesign <info@nieminen.design>
2025-07-06 02:17:51 +0300

    Update Start.jsx

--- src/views/Start.jsx
+++ src/views/Start.jsx
@@ -1,1 +1,10 @@
-// start page
+import React from 'react';
+
+export default function Start() {
+  return (
+    <div style={{ padding: "1rem", fontFamily: "sans-serif" }}>
+      <h1>Tervetuloa Tuloscorttiin!</h1>
+      <p>Valitse kenttä ja aloita kierros</p>
+    </div>
+  );
+}
