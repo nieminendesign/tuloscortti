07da0876b84db17e337773e0872f83106e3fb836
nieminendesign <info@nieminen.design>
2025-07-06 02:17:22 +0300

    Update App.jsx

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,1 +1,11 @@
-// routes
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Start from '../views/Start.jsx';
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Start />} />
+    </Routes>
+  );
+}
